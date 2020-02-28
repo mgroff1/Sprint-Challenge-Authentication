ae6d4c4e5953a338b7e7e66d385efb993b5ad9f1
Mick Groff <mick.groff@gmail.com>
2020-02-28 10:06:10 -0700

    gettingthere-added-some-tests

--- api/server.js
+++ api/server.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const configureMiddleware = require("./configureMiddleware.js");
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const {
+    restricted
+} = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+const jokesRouter = require("../jokes/jokes-router.js");
 
 const server = express();
+configureMiddleware(server);
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", restricted, jokesRouter);
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.get("/api", (req, res) => {
+    res.json({
+        message: `API Working!!!!!`
+    });
+});
 
 module.exports = server;
+
